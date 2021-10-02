89aa0cf408f6f62b77fc85ec8858b658050b3c65
Juan E. Sanchez <juan@tcad.com>
2021-10-02 16:38:18 -0500

    fix script

--- ieee/sweeps.sh
+++ ieee/sweeps.sh
@@ -4,11 +4,11 @@ set -euxo pipefail
 RESULTDIR=${PWD}/testdir
 mkdir -p ${RESULTDIR}
 
-(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_hfs_id_vd.txt --hfs ) |& tee ${RESULTDIR}/bulk_hfs_id_vd.out
-(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv darwish_id_vd.txt --hfs ) |& tee ${RESULTDIR}/darwish_id_vd.out
-(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_id_vd.txt --no-hfs ) |& tee ${RESULTDIR}/bulk_id_vd.out
+(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv ${RESULTDIR}/bulk_hfs_id_vd.txt --hfs ) |& tee ${RESULTDIR}/bulk_hfs_id_vd.out
+(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv ${RESULTDIR}/darwish_id_vd.txt --hfs ) |& tee ${RESULTDIR}/darwish_id_vd.out
+(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv ${RESULTDIR}/bulk_id_vd.txt --no-hfs ) |& tee ${RESULTDIR}/bulk_id_vd.out
 
-(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_id_vg.txt --no-hfs ) |& tee ${RESULTDIR}/bulk_id_vg.out
-(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_hfs_id_vg.txt --hfs ) |& tee ${RESULTDIR}/bulk_hfs_id_vg.out
-(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv darwish_id_vg.txt --hfs ) |& tee ${RESULTDIR}/darwish_id_vg.out
+(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv ${RESULTDIR}/bulk_id_vg.txt --no-hfs ) |& tee ${RESULTDIR}/bulk_id_vg.out
+(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv ${RESULTDIR}/bulk_hfs_id_vg.txt --hfs ) |& tee ${RESULTDIR}/bulk_hfs_id_vg.out
+(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv ${RESULTDIR}/darwish_id_vg.txt --hfs ) |& tee ${RESULTDIR}/darwish_id_vg.out
 
