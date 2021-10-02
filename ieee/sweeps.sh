b47933c447f97f75d418f75d4fb2e8e2c63830b6
Juan E. Sanchez <juan@tcad.com>
2021-10-02 16:26:17 -0500

    update script for output dir

--- ieee/sweeps.sh
+++ ieee/sweeps.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 set -euxo pipefail
-#(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_hfs_id_vd.txt --hfs ) |& tee bulk_hfs_id_vd.out
-#(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv darwish_id_vd.txt --hfs ) |& tee darwish_id_vd.out
-#(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_id_vd.txt --no-hfs ) |& tee bulk_id_vd.out
 
-#(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_id_vg.txt --no-hfs ) |& tee bulk_id_vg.out
-(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_hfs_id_vg.txt --hfs ) |& tee bulk_hfs_id_vg.out
-(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv darwish_id_vg.txt --hfs ) |& tee darwish_id_vg.out
+RESULTDIR=${PWD}/testdir
+mkdir -p ${RESULTDIR}
 
-#(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv mu_ratio.txt --hfs ) |& tee mu_ratio.out
+(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_hfs_id_vd.txt --hfs ) |& tee ${RESULTDIR}/bulk_hfs_id_vd.out
+(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv darwish_id_vd.txt --hfs ) |& tee ${RESULTDIR}/darwish_id_vd.out
+(time python idvd.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_id_vd.txt --no-hfs ) |& tee ${RESULTDIR}/bulk_id_vd.out
+
+(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_id_vg.txt --no-hfs ) |& tee ${RESULTDIR}/bulk_id_vg.out
+(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model bulk  --csv bulk_hfs_id_vg.txt --hfs ) |& tee ${RESULTDIR}/bulk_hfs_id_vg.out
+(time python sweeps.py --gmsh ./cubit02_interface.gmsh  --model darwish  --csv darwish_id_vg.txt --hfs ) |& tee ${RESULTDIR}/darwish_id_vg.out
+
